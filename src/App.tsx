48b968e5766329918f89cab12f40ef0fb4382db1
Sheila Sheikh <166654065+sheila-semrush@users.noreply.github.com>
2026-02-16 22:23:26 +0100

    added date picker examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
-import { Flex } from '@semcore/base-components';
-import { DatePicker, DateRangePicker } from '@semcore/date-picker';
+import {
+  DatePicker,
+  DateRangeComparator,
+  DateRangePicker,
+  MonthDateRangeComparator,
+  MonthPicker,
+  MonthRangePicker,
+} from '@semcore/date-picker';
 import { Text } from '@semcore/typography';
 import './App.css';
 import React from 'react';
+import { Flex } from '@semcore/base-components';
 
 const Demo = () => {
-  const [value, setValue] = React.useState(new Date('06/29/2020'));
-  const [valueRange, setValueRange] = React.useState([]);
-
   return (
-    <Flex gap={5} flexWrap>
-      <Flex direction='column'>
-        <Text tag='label' size={200} htmlFor='simple-date-picker'>
-          Simple date picker
+    <>
+      <h1>Welcome to the DatePicker testing page</h1>
+
+      <Flex direction='column' alignItems='start' gap={2}>
+        <Text tag='label' size={300} htmlFor='date-picker-1' mt={4}>
+          Single date
         </Text>
-        <DatePicker value={value} onChange={(date: any) => setValue(date)}>
-          <DatePicker.Trigger mt={2} id='simple-date-picker' />
+        <DatePicker size='l'>
+          <DatePicker.Trigger id='date-picker-1' />
           <DatePicker.Popper />
         </DatePicker>
-      </Flex>
-      <Flex direction='column'>
-        <Text tag='label' size={200} htmlFor='simple-date-range-picker'>
-          Date range picker
+
+        <Text tag='label' size={300} htmlFor='date-picker-2' mt={4}>
+          Date range
         </Text>
-        <DateRangePicker value={valueRange} onChange={(date: any) => setValueRange(date)}>
-          <DateRangePicker.Trigger mt={2} id='simple-date-range-picker' />
+        <DateRangePicker>
+          <DateRangePicker.Trigger size='l' id='date-picker-2' />
           <DateRangePicker.Popper />
         </DateRangePicker>
+
+        <Text tag='label' size={300} htmlFor='date-picker-3' mt={4}>
+          Single month
+        </Text>
+        <MonthPicker size='l'>
+          <MonthPicker.Trigger id='date-picker-3' />
+          <MonthPicker.Popper />
+        </MonthPicker>
+
+        <Text tag='label' size={300} htmlFor='date-picker-4' mt={4}>
+          Month range
+        </Text>
+        <MonthRangePicker>
+          <MonthRangePicker.Trigger size='l' id='date-picker-4' />
+          <MonthRangePicker.Popper />
+        </MonthRangePicker>
+
+        <DateRangeComparator>
+          <DateRangeComparator.Trigger placeholder='Compare date ranges' mt={4} />
+          <DateRangeComparator.Popper />
+        </DateRangeComparator>
+
+        <MonthDateRangeComparator>
+          <MonthDateRangeComparator.Trigger placeholder='Compare month ranges' mt={4} />
+          <MonthDateRangeComparator.Popper />
+        </MonthDateRangeComparator>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
